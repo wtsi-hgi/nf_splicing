521aadad6fb1cbaf9ce76cd7cacc2fce5362f44c
fei-hgi <fs18.sanger.ac.uk>
2025-04-02 15:36:35 +0100

    add codes for fitltering steps

--- modules/filtering_reads_se.nf
+++ modules/filtering_reads_se.nf
@@ -1,21 +1,34 @@
-workflow filtering_reads {
+workflow filtering_reads_se {
     take:
     ch_sample
 
     main:
+    bwa_align_se_reads(ch_sample)
+    ch_bwa_se_unmapped = bwa_align_se_reads.out.ch_bwa_se_unmapped
+    ch_bwa_se_bam = bwa_align_se_reads.out.ch_bwa_se_bam
+    
+    filter_se_reads(ch_bwa_se_bam)
+    ch_bwa_se_wrongmap = filter_se_reads.out.ch_bwa_se_wrongmap
+    ch_bwa_se_filtered = filter_se_reads.out.ch_bwa_se_filtered
 
+    ch_bwa_se_joined = ch_bwa_se_unmapped.join(ch_bwa_se_wrongmap)
+    merge_fastqs(ch_bwa_se_joined)
+    ch_bwa_se_fail = merge_fastqs.out.ch_bwa_se_fail
 
     emit:
+    ch_bwa_se_filtered
+    ch_bwa_se_fail
 }
 
 process bwa_align_se_reads {
-    label 'process_low'
+    label 'process_medium'
 
     input:
     tuple val(sample_id), path(barcode), path(extended_frags), path(exon_fasta)
 
     output:
-    tuple val(sample_id), path("${sample_id}.filter_se.fail.fastq.gz"), emit: ch_bwa_se
+    tuple val(sample_id), path("${sample_id}.filter_se.unmapped.fastq.gz"), emit: ch_bwa_se_unmapped
+    tuple val(sample_id), path("${sample_id}.filter_se.unique.sorted.bam"), path("${sample_id}.filter_se.unique.sorted.bam.bai"), emit: ch_bwa_se_bam
 
     script:
     """
@@ -25,7 +38,43 @@ process bwa_align_se_reads {
     samtools sort -@ 32 -o ${sample_id}.filter_se.unique.sorted.bam ${sample_id}.filter_se.unique.bam
     samtools index ${sample_id}.filter_se.unique.sorted.bam
     rm ${sample_id}.filter_se.sam ${sample_id}.filter_se.unique.bam
-
     """
 }
 
+process filter_se_reads {
+    label 'process_high'
+
+    input:
+    tuple val(sample_id), path(bam), path(bai)
+
+    output:
+    tuple val(sample_id), path("${sample_id}.filter_se.wrongmap.fastq.gz"), emit: ch_bwa_se_wrongmap
+    tuple val(sample_id), path("${sample_id}.filter_se.filtered.sorted.bam"), path("${sample_id}.filter_se.filtered.sorted.bam.bai"), emit: ch_bwa_se_filtered
+
+    script:
+    """
+    ${projectDir}/scripts/filter_bam_se.R -i ${bam}
+    samtools view -@ 80 -b -o ${sample_id}.filter_se.filtered.bam ${sample_id}.filter_se.unique.sorted.filtered.sam
+    samtools sort -@ 80 -o ${sample_id}.filter_se.filtered.sorted.bam ${sample_id}.filter_se.filtered.bam
+    samtools index -@ 80 ${sample_id}.filter_se.filtered.sorted.bam
+    rm ${sample_id}.filter_se.filtered.bam ${sample_id}.filter_se.unique.sorted.filtered.sam
+    
+    mv ${sample_id}.filter_se.unique.sorted.wrongmap.fastq ${sample_id}.filter_se.wrongmap.fastq
+    pigz --best -p 80 ${sample_id}.filter_se.wrongmap.fastq
+    """
+}
+
+process merge_fastqs {
+    label 'process_single'
+
+    input:
+    tuple val(sample_id), path(unmapped_fastq), path(wrongmap_fastq)
+
+    output:
+    tuple val(sample_id), path("${sample_id}.filter_se.fail.fastq.gz"), emit: ch_bwa_se_fail
+
+    script:
+    """
+    cat ${unmapped_fastq} ${wrongmap_fastq} > ${sample_id}.filter_se.fail.fastq.gz
+    """
+}
---
+++ scripts/filter_bam_se.R
@@ -0,0 +1,168 @@
+#!/usr/bin/env Rscript
+quiet_library <- function(pkg) { suppressMessages(suppressWarnings(library(pkg, character.only = TRUE))) }
+packages <- c("tidyverse", "data.table", "Rsamtools", "Biostrings", "parallel", "optparse")
+invisible(lapply(packages, quiet_library))
+
+#-- options --#
+option_list <- list(make_option(c("-b", "--input_bam"),  type = "character", help = "input bam"),
+                    make_option(c("-s", "--softclip"),   type = "integer",   help = "softclip tolerance", default = 5),
+                    make_option(c("-o", "--output_dir"), type = "character", help = "output directory",   default = getwd()),
+                    make_option(c("-t", "--threads"),    type = "integer",   help = "number of threads",  default = 64),
+                    make_option(c("-c", "--chunk_size"), type = "integer",   help = "chunk size",         default = 10000))
+# Parse arguments
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+
+# Check if required arguments are provided
+if(is.null(opt$input_bam)) stop("-b, input bam file is required!", call. = FALSE)
+
+#-- function --#
+calc_end_pos <- function(start_pos, cigar)
+{
+    tb_cigar <- tibble(cigar = cigar) %>%
+                  mutate(values = str_extract_all(cigar, "\\d+"), operations = str_extract_all(cigar, "[A-Z]")) %>%  
+                  unnest(c(values, operations)) %>%  # Expand the list columns
+                  mutate(value = as.numeric(values)) %>%
+                  select(-values)
+
+    valid_ops <- c("M", "D", "N")
+
+    total_ref_length <- tb_cigar %>%
+                          filter(operations %in% valid_ops) %>%
+                          summarise(ref_length = sum(value, na.rm = TRUE)) %>%
+                          pull(ref_length)
+    
+    end_pos <- start_pos + total_ref_length - 1
+    return(end_pos)
+}
+
+calc_softclip_lens <- function(cigar)
+{
+    first_softclip <- 0
+    last_softclip <- 0
+    
+    if(grepl("^[0-9]+S", cigar)) first_softclip <- as.numeric(sub("S", "", regmatches(cigar, regexpr("^[0-9]+S", cigar))))
+    if(grepl("[0-9]+S$", cigar)) last_softclip <- as.numeric(sub("S", "", regmatches(cigar, regexpr("[0-9]+S$", cigar))))
+
+    return(c(first_softclip, last_softclip))
+}
+
+write_to_sam_file <- function(bam_data, index, output_sam)
+{
+    read_out <- c(bam_data$qname[index],
+                  bam_data$flag[index],
+                  as.character(bam_data$rname[index]),
+                  bam_data$pos[index],
+                  bam_data$mapq[index],
+                  bam_data$cigar[index],
+                  ifelse(is.na(bam_data$mrnm[index]), "*", as.character(bam_data$mrnm[index])),
+                  ifelse(is.na(bam_data$mpos[index]), 0, bam_data$mpos[index]),
+                  bam_data$isize[index],
+                  data.frame(bam_data$seq[index])[1,1],
+                  data.frame(bam_data$qual[index])[1,1],
+                  paste0("NM:i:", bam_data$tag$NM[index]),
+                  paste0("AS:i:", bam_data$tag$AS[index]),
+                  paste0("XS:i:", bam_data$tag$XS[index]))
+
+    fwrite(as.data.frame(t(read_out)), output_sam, append = TRUE, quote = FALSE, sep = '\t', row.names = FALSE, col.names = FALSE)
+}
+
+process_read <- function(i, bam_data)
+{
+    # if(i %% 1000 == 0) cat("\r", paste0("  |--> processed: ", i, "/", length(bam_data[[1]]$seq)))
+
+    read_name <- bam_data[[1]]$qname[i]
+    read_start_pos <- bam_data[[1]]$pos[i]
+    read_cigar <- bam_data[[1]]$cigar[i]
+    read_end_pos <- calc_end_pos(read_start_pos, read_cigar)
+    read_ref <- bam_data[[1]]$rname[i]
+    read_seq <- data.frame(bam_data[[1]]$seq[i])[1,1]
+    read_qual <- data.frame(bam_data[[1]]$qual[i])[1,1]
+
+    # Note:
+    # 1. read alignment should be end-to-end, allow 5 bp difference for softclip
+    # 2. library may be longer than reads, so reads may not cover the end part of last exon, allow 5 bp difference
+    read_start_pass <- ((exon_positions$exon_end[1] - read_start_pos) > pos_tolerance)
+    read_end_pass <- ifelse(read_ref == "e10_e11_e12", 
+                            ((read_end_pos - sum(exon_positions$length[c(1,2)]) + 1) > pos_tolerance), 
+                            ((read_end_pos - sum(exon_positions$length[1]) + 1) > pos_tolerance))
+    if(read_start_pass & read_end_pass)
+    {
+        write_to_sam_file(bam_data[[1]], i, output_sam)
+    } else {
+        read_out <- c(paste0("@", read_name), read_seq, "+", read_qual)
+        fwrite(as.data.frame(read_out), output_fastq, append = TRUE, quote = FALSE, row.names = FALSE, col.names = FALSE)
+    }
+}
+
+#-- inputs --#
+bam_file <- opt$input_bam
+pos_tolerance <- opt$softclip
+num_cores <- opt$threads
+bam_chunk_size <- opt$chunk_size
+
+bam_prefix <- tools::file_path_sans_ext(basename(opt$input_bam))
+
+exon_positions <- rbind(c("E5", 1, 62),
+                        c("E6", 215, 277),
+                        c("E7", 410, 515))
+colnames(exon_positions) <- c("exon_id", "exon_start", "exon_end")
+exon_positions <- as.data.table(exon_positions)
+set(exon_positions, j = "exon_id", value = as.character(exon_positions$exon_id))
+set(exon_positions, j = "exon_start", value = as.integer(exon_positions$exon_start))
+set(exon_positions, j = "exon_end", value = as.integer(exon_positions$exon_end))
+exon_positions$length <- exon_positions$exon_end - exon_positions$exon_start + 1
+
+#-- outputs --#
+if(!dir.exists(opt$output_dir)) dir.create(opt$output_dir, recursive = TRUE)
+setwd(opt$output_dir)
+
+output_sam <- paste0(bam_prefix, ".filtered.sam")
+output_fastq <- paste0(bam_prefix, ".wrongmap.fastq")
+if(file.exists(output_sam)) invisible(file.remove(output_sam))
+if(file.exists(output_fastq)) invisible(file.remove(output_fastq))
+
+#-- processing --#
+header <- scanBamHeader(bam_file)[[1]]$text
+for(i in 1:length(header))
+{
+    if(names(header[i]) == "@HD" || names(header[i]) == "@SQ")
+    {
+        line <- c(names(header[i]), unlist(header[i]))
+        fwrite(as.data.table(t(line)),
+               output_sam,
+               append = TRUE,
+               quote = FALSE,
+               sep = '\t',
+               row.names = FALSE,
+               col.names = FALSE)
+    }
+}
+
+bam_fields <- c("qname", "flag", "rname", "pos", "mapq", "cigar", "mrnm", "mpos", "isize", "seq", "qual")
+tag_fields <- c("NM", "AS", "XS")
+param <- ScanBamParam(what = bam_fields, tag = tag_fields)
+
+bam_read_handle <- open(BamFile(bam_file, yieldSize = bam_chunk_size))
+bam_chunk_index <- 1
+repeat
+{
+    bam_chunk <- scanBam(bam_read_handle, param = param, nThreads = num_cores)
+    if(length(bam_chunk[[1]]$seq) == 0)
+    {
+        cat("done!\n")
+        break
+    } else {
+        cat("\n")
+        range_start <- format((bam_chunk_index - 1) * bam_chunk_size + 1, scientific = FALSE)
+        range_end <- format((bam_chunk_index - 1) * bam_chunk_size + length(bam_chunk[[1]]$seq), scientific = FALSE)
+        cat(paste0("processing chunk: ", range_start, " ~ ", range_end, "\n"))
+        bam_chunk_index <- bam_chunk_index + 1
+    }
+
+    mclapply(1:length(bam_chunk[[1]]$seq), 
+             function(i) process_read(i, bam_chunk), 
+             mc.cores = num_cores)
+}
+
+close(bam_read_handle)
