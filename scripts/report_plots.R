ec1952d1e6e71d71acee8ca1ce70956830ddf1f7
fei-hgi <fs18.sanger.ac.uk>
2025-09-25 14:16:10 +0100

    add r functions to create figures

--- scripts/report_plots.R
+++ scripts/report_plots.R
@@ -1,4 +1,4 @@
-create_barplots <- function(sample_reps, total_reads, merged_reads, unmerged_reads, inclusion_reads, skipping_reads, map_reads, unexplain_reads)
+create_barplots <- function(barcode_association, sample_reps, total_reads, merged_reads, unmerged_reads, inclusion_reads, skipping_reads, map_reads, unexplain_reads)
 {
     summary_reads <- as.data.table(cbind(sample_reps, total_reads, merged_reads, unmerged_reads, inclusion_reads, skipping_reads, map_reads, unexplain_reads))
     colnames(summary_reads) <- c("sample_reps", "total_reads", "merged_reads", "unmerged_reads", "inclusion_reads", "skipping_reads", "map_reads", "unexplain_reads")
@@ -95,24 +95,112 @@ create_junction_plots <- function(classified_junctions)
     dt_junctions <- Reduce(function(x, y) merge(x, y, by = c("chrom", "donor", "acceptor", "annotation"), all = TRUE), dt_list)
     
     cov_cols <- setdiff(names(dt_junctions), c("chrom", "donor", "acceptor", "annotation"))
-    dt_junctions[rowSums(!is.na(dt_junctions[, ..cov_cols])) >= 2]
+    dt_junctions <- dt_junctions[rowSums(!is.na(dt_junctions[, ..cov_cols])) >= 2]
     dt_junctions[, avg_cov := rowMeans(.SD, na.rm = TRUE), .SDcols = cov_cols]
     setnames(dt_junctions, "chrom", "var_id")
 
     return(list(p, dt_junctions))
 }
 
-rescale_junctions <- function(exons, junctions, scale_range = c(1, 700))
-{
-    bounds <- exons[, .(old_min = min(exon_start), old_max = max(exon_end)), by = var_id]
-    bounds[, `:=`(scale_min = scale_range[1], scale_max = scale_range[2], scale_len = scale_range[2] - scale_range[1])]
-    bounds[, old_len := old_max - old_min]
+rescale_junctions <- function(junctions, exons, lib_type)
+{   
+    exon_template <- exons[1:3,]
+    introns_template <- exon_template[, .(intron_id   = paste0("I", .I - 1),
+                                          intron_start = shift(exon_end, type = "lag") + 1,
+                                          intron_end   = exon_start - 1), by = .(var_id)][!is.na(intron_start)]
 
-    junctions[,  var_id_scale := sub("(/.*)$", "", var_id)]
-    junctions_scale <- merge(junctions, bounds, by = "var_id", allow.cartesian = TRUE)
-    junctions_scale[, donor_scale := round(scale_min + (donor - old_min) * (scale_len / old_len))]
-    junctions_scale[, acceptor_scale := round(scale_min + (acceptor - old_min) * (scale_len / old_len))]
-  
-    return(junctions_scale)
+    if(lib_type == "random_intron" || lib_type == "random_exon")
+    {
+        return(list(junctions, exon_template, introns_template))
+    }
+    
+    junctions_tmp <- copy(junctions)
+    junctions_tmp[, var_id := sub("(/.*)$", "", var_id)]
+
+    if(lib_type == "muta_exon")
+    {
+        targets <- copy(exons)
+        setnames(targets, c("exon_id", "exon_start", "exon_end"), c("target_id", "target_start", "target_end"))
+        targets <- merge(targets, 
+                         exon_template[, .(target_id = exon_id, template_start = exon_start, template_end = exon_end)], 
+                         by = "target_id", 
+                         all.x = TRUE)
+
+    } else {
+        targets <- exons[, .(target_id    = paste0("I", seq_len(.N) - 1),
+                             target_start = shift(exon_end, type = "lag") + 1,
+                             target_end   = exon_start - 1), by = var_id][!is.na(target_start)]
+        targets <- merge(targets, 
+                         introns_template[, .(target_id = intron_id, template_start = intron_start, template_end = intron_end)], 
+                         by = "target_id", 
+                         all.x = TRUE)
+    }
+    
+    junctions_tmp <- merge(junctions_tmp, targets, by = "var_id", allow.cartesian = TRUE)
+
+    junctions_tmp[, donor_rescale := fifelse(donor >= target_start & donor <= target_end,
+                             round(template_start + (donor - target_start) / (target_end - target_start) * (template_end - template_start)),
+                             donor)]
+
+    junctions_tmp[, acceptor_rescale := fifelse(acceptor >= target_start & acceptor <= target_end,
+                             round(template_start + (acceptor - target_start) / (target_end - target_start) * (template_end - template_start)),
+                             acceptor)]
+
+    junctions_tmp <- junctions_tmp[, .(donor_rescale = min(donor_rescale), acceptor_rescale = min(acceptor_rescale)), 
+                                    by = .(var_id, donor, acceptor, annotation, I4_Rep1, I4_Rep2, I4_Rep3, avg_cov)]
+    junctions_tmp <- junctions_tmp %>% select(-c(donor, acceptor))
+    setnames(junctions_tmp, c("donor_rescale", "acceptor_rescale"), c("donor", "acceptor"))
+    setcolorder(junctions_tmp, colnames(junctions))
+
+    return(list(junctions, exon_template, introns_template))
 }
 
+create_junction_distribution <- function(junctions, exons, introns)
+{
+    junctions_a1b10 <- junctions %>% filter(avg_cov >= 1 & avg_cov < 10)
+    junctions_a10b100 <- junctions %>% filter(avg_cov >= 10 & avg_cov < 100)
+    junctions_a100b1000 <- junctions %>% filter(avg_cov >= 100 & avg_cov < 1000)
+    junctions_a1000b10000 <- junctions %>% filter(avg_cov >= 1000 & avg_cov < 10000)
+    junctions_a10000 <- junctions %>% filter(avg_cov >= 10000)
+
+    list_junctions <- list(junctions, junctions_a1b10, junctions_a10b100, junctions_a100b1000, junctions_a1000b10000, junctions_a10000)
+    names(list_junctions) <- c("avg_cov:all", "avg_cov:1-10", "avg_cov:10-100", "avg_cov:100-1000", "avg_cov:1000-10000", "avg_cov:10000+")
+    list_diagramplots <- list()
+    list_scatterplots <- list()
+
+    for(i in seq_along(list_junctions))
+    {
+        p1 <- ggplot() +
+                geom_rect(data = exons, fill = t_col("darkgreen", 0.6), color = t_col("darkgreen", 0.6), aes(xmin = exon_start, xmax = exon_end, ymin = 0.8, ymax = 1)) +
+                geom_rect(data = introns, fill = "grey", color = "grey", aes(xmin = intron_start, xmax = intron_end, ymin = 0.87, ymax = 0.93)) +
+                geom_curve(data = list_junctions[[i]], aes(x = donor, xend = acceptor, y = 1, yend = 1, color = avg_cov, alpha = avg_cov), curvature = -0.5, linewidth = 0.2, lineend = "round") +
+                scale_color_gradient(trans = "log10", low = "blue", high = "brown1", labels = label_number(accuracy = 1)) +
+                scale_alpha(trans = "log10", range = c(0.01, 1)) + guides(alpha = "none") +
+                coord_cartesian(xlim = c(0, max(exons$exon_end)), ylim = c(0, 3)) +
+                theme_void() + theme(legend.position = "left")
+
+        p2 <- ggplot(list_junctions[[i]], aes(x = donor, y = acceptor, color = avg_cov)) +
+                theme_minimal() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+                geom_vline(data = exons, aes(xintercept = exon_start), color = "lightgrey", linetype = "dashed") +
+                geom_vline(data = exons, aes(xintercept = exon_end), color = "lightgrey", linetype = "dashed") +
+                geom_hline(data = exons, aes(yintercept = exon_start), color = "lightgrey", linetype = "dashed") +
+                geom_hline(data = exons, aes(yintercept = exon_end), color = "lightgrey", linetype = "dashed") +
+                labs(x = "Start", y = "End", size = "Average") +
+                geom_point(shape = 19, alpha = 1, size = 0.5) +
+                scale_color_continuous(trans = "log10", low = "blue", high = "brown1", labels = label_number(accuracy = 1)) +
+                scale_x_continuous(limits = c(-10, max(exons$exon_end)), breaks = seq(0, max(exons$exon_end), by = 100)) +  
+                scale_y_continuous(limits = c(-10, max(exons$exon_end)), breaks = seq(0, max(exons$exon_end), by = 100)) + 
+                theme(axis.text.x = element_text(angle = 45, hjust = 1)) + theme(legend.position = "left") +
+                geom_rect(data = exons, inherit.aes = FALSE, fill = "darkgreen", alpha = 0.6, aes(xmin = exon_start, xmax = exon_end, ymin = -10, ymax = 0)) + 
+                geom_rect(data = exons, inherit.aes = FALSE, fill = "darkgreen", alpha = 0.6, aes(ymin = exon_start, ymax = exon_end, xmin = -10, xmax = 0)) +
+                geom_segment(data = introns, inherit.aes = FALSE, fill = "grey", alpha = 0.6, aes(x = intron_start, xend = intron_end, y = -5, yend = -5)) +
+                geom_segment(data = introns, inherit.aes = FALSE, fill = "grey", alpha = 0.6, aes(y = intron_start, yend = intron_end, x = -5, xend = -5))
+
+        p3 <- ggMarginal(p2, type = "density", margins = "both", color = "brown1", fill = "pink", size = 8)
+
+        list_diagramplots[[i]] <- p1
+        list_scatterplots[[i]] <- p3
+    }
+
+    return(list(list_junctions, list_diagramplots, list_scatterplots))
+}
